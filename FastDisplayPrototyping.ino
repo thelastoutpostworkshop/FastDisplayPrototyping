4f8a7ed270baa70f496a8d1dfcfac14dc8d45236
charles <cgiguere@iname.com>
2023-04-14 19:27:47 -0400

    test

--- FastDisplayPrototyping.ino
+++ FastDisplayPrototyping.ino
@@ -18,15 +18,25 @@ void setup()
   // tft.setFreeFont(LABEL2_FONT);
 
   // Buffer Limit from the Serial Monitor is 64 bytes
-  sDisplay.runCommands(F("x;ts1;sc10,10;#17e0;thDisplay Prototyping;"));
-  sDisplay.runCommands(F("sc10,30;ts1;thLibrary v1.0;lh0,22,w"));
-  sDisplay.runCommands(F("sc10,10;tvVertical Center;sc10,85;ttNormal text;"));
-  sDisplay.runCommands(F("#f804;ch20,60,10;#ffe0;cf50,60,10;"));
-  sDisplay.runCommands(F("#05ff;gh70,50,90,60,70,70;#ffff;gf100,50,120,60,100,70;"));
-  sDisplay.runCommands(F("#fc7c;rh130,50,20,20;#263c;rf160,50,20,20;#efe0;ri10,100,40,40,10;"));
-  sDisplay.runCommands(F("#f9e0;rj60,100,40,40,13;#1f9d;lv2,45,280;#07ff;lh2,170,240;"));
-  sDisplay.runCommands(F("#ded8;dl2,170,240,320;"));
-  sDisplay.runCommands(F("ca50,50,50,40,0,180,00ff,ffff,1"));
+  // sDisplay.runCommands(F("x;ts1;sc10,10;#17e0;thDisplay Prototyping;"));
+  // sDisplay.runCommands(F("sc10,30;ts1;thLibrary v1.0;lh0,22,w"));
+  // sDisplay.runCommands(F("sc10,10;tvVertical Center;sc10,85;ttNormal text;"));
+  // sDisplay.runCommands(F("#f804;ch20,60,10;#ffe0;cf50,60,10;"));
+  // sDisplay.runCommands(F("#05ff;gh70,50,90,60,70,70;#ffff;gf100,50,120,60,100,70;"));
+  // sDisplay.runCommands(F("#fc7c;rh130,50,20,20;#263c;rf160,50,20,20;#efe0;ri10,100,40,40,10;"));
+  // sDisplay.runCommands(F("#f9e0;rj60,100,40,40,13;#1f9d;lv2,45,280;#07ff;lh2,170,240;"));
+  // sDisplay.runCommands(F("#ded8;dl2,170,240,320;"));
+
+  sDisplay.runCommands(F("x;#00ff;"));
+  sDisplay.runCommands(F("sc1,1;ttHello;"));
+  sDisplay.runCommands(F("ts2;ttWorld!;"));
+  sDisplay.runCommands(F("#ff00;dp100,10;"));
+  sDisplay.runCommands(F("dl2,20,2,h;"));
+  sDisplay.runCommands(F("#005f;ch100,10,10;"));
+  sDisplay.runCommands(F("#f104;cf120,10,10;"));
+  sDisplay.runCommands(F("#f7c2;ce110,75,10,50;"));
+  sDisplay.runCommands(F("#12df;cg90,75,10,50;"));
+  sDisplay.runCommands(F("ca80,80,50,45,300,345,f8a2,00ff,0;"));
 
   // tft.fillScreen(TFT_BLACK);
   // tft.fillRectHGradient(10,50,50,50,0x72ff,0xd2ff);
