142f4690b99aea47e8ce6fbb6b43954ca3ac7283
charles <cgiguere@iname.com>
2023-04-16 07:49:25 -0400

    gfx example

--- examples/GFX/GFX.ino
+++ examples/GFX/GFX.ino
@@ -14,6 +14,16 @@ Documentation : https://github.com/thelastoutpostworkshop/FastDisplayPrototyping
 #define OUTPUT_CODE_ON_SERIAL   // Output graphical functions on the Serial Monitor, comment it to disable
 #include <FastDisplayPrototyping.h>
 
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
 // Initialize the display
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
