9369fd9b9ba4519e1672368285823f7bdd0748cf
charles <cgiguere@iname.com>
2023-04-20 06:33:19 -0400

    GFX example

--- examples/GFX/GFX.ino
+++ examples/GFX/GFX.ino
@@ -9,24 +9,27 @@ Tutorial :
 Documentation : https://github.com/thelastoutpostworkshop/FastDisplayPrototyping
 ***/
 
-#include <Adafruit_TFTLCD.h>    // Include the hardware-specific library for your type of display
-#include <Adafruit_GFX.h>       // Core graphics library
+// Core graphics library
+#include <Adafruit_GFX.h>     
 
-#define OUTPUT_CODE_ON_SERIAL   // Output graphical functions on the Serial Monitor, comment it to disable
-#include <FastDisplayPrototyping.h>
+// Hardware-specific library
+#include <Adafruit_TFTLCD.h>  
 
+// Hardware-specific definitions
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
-
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-// Initialize the display
-Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); // Initialize the display
+
+// Fast Display Prototyping library
+#define OUTPUT_CODE_ON_SERIAL // Output graphical functions on the Serial Monitor, comment it to disable
+#include <FastDisplayPrototyping.h>
 
 // Initialize the display prototyping library
-FastSerialDisplay sDisplay(&tft,"tft"); 
+FastSerialDisplay sDisplay(&tft, "tft");
 
 void setup() {
   Serial.begin(9600); // This line mandatory for using the display prototyping library, change the baud rate if needed
