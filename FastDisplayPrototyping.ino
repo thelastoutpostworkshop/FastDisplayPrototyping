7b5d7115773cd4e1b2e22f9438170691b2e3cefc
charles <cgiguere@iname.com>
2023-04-20 06:28:37 -0400

    test

--- FastDisplayPrototyping.ino
+++ FastDisplayPrototyping.ino
@@ -1,19 +1,23 @@
 // GFX Test
 
-#include <Adafruit_TFTLCD.h>  // Hardware-specific library
-#include <Adafruit_GFX.h>     // Core graphics library
+// Core graphics library
+#include <Adafruit_GFX.h>     
+
+// Hardware-specific library
+#include <Adafruit_TFTLCD.h>  
+
+// Fast Display Prototyping library
 #define OUTPUT_CODE_ON_SERIAL // Output graphical functions on the Serial Monitor, comment it to disable
 #include "src/FastDisplayPrototyping.h"
 
+// Hardware-specific definitions
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
-
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
 
-// Initialize the display
-Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); // Initialize the display
 
 // Initialize the display prototyping library
 FastSerialDisplay sDisplay(&tft, "tft");
